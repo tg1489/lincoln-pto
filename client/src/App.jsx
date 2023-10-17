02155c36f0acf9947ee12d1d514cda505e62d4e3
Tony <tonyguarino1489@gmail.com>
2023-10-16 21:03:00 -0400

    Decent design for header. Still not complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react';
-import 'bootstrap';
+import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <>
+      <Header />
       <h3>WeLCoMe 2 liNC0lnn skOoL</h3>
     </>
   );
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,79 @@
 import React from 'react';
+import lion from '../assets/lion-logo2.png';
+import '../Header.css';
 
 export default function Header() {
   return (
-    <div>
-      <h2>This is my header</h2>
-    </div>
+    <>
+      <div className='container-fluid header'>
+        <div className='row'>
+          <div className='col'>
+            <span>Home</span>
+            <span>PTO Meeting Schedule</span>
+            <span>Birthdays</span>
+            <span>Store</span>
+            <span>Volunteers</span>
+            <span>Events</span>
+            <span>Fundraising</span>
+            <span>About Us</span>
+            <span>Contact Us</span>
+            <img />
+            <img />
+            <span>Login</span>
+          </div>
+        </div>
+        <div className='row'>
+          <div className='col-1'>
+            <img src={lion} className='logo' alt='lincoln-lion-logo' />
+          </div>
+          <div className='col-3 school-name'>Lincoln Elementary PTO</div>
+        </div>
+      </div>
+    </>
   );
 }
+
+/*
+
+
+
+
+
+
+<nav className='nav navbar navbar-expand-sm mb-3'>
+        <div className='container-fluid'>
+          <div className='row'></div>
+          <div className='navbar-brand'>
+            <img src={lion} className='logo' alt='lincoln-lion-logo' />
+          </div>
+
+          <span>Lincoln Elementary PTO</span>
+        </div>
+      </nav>
+
+
+
+
+
+
+
+
+
+
+
+ <div className='col'>Home</div>
+            <div className='col'>PTO Meeting Schedule</div>
+            <div className='col'>Birthdays</div>
+            <div className='col'>Store</div>
+            <div className='col'>Volunteers</div>
+            <div className='col'>Events</div>
+            <div className='col'>Fundraising</div>
+            <div className='col'>About Us</div>
+            <div className='col'>Contact Us</div>
+            <img />
+            <img />
+            <div className='col'>Login</div>
+          </div>
+
+
+*/
