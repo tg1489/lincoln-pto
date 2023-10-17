5002476fe7446c0fc0a3dda4a07992cf6355bbfa
Tony <tonyguarino1489@gmail.com>
2023-10-17 18:46:43 -0400

    Fixed join us section for mobile

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Modal } from 'react-bootstrap';
+import youtube from '../assets/yt.png';
+import facebook from '../assets/fb.png';
+import twitter from '../assets/x.png';
+import email from '../assets/email.png';
+import donate from '../assets/donate.png';
 import '../Footer.css';
 
 export default function Footer() {
   return (
     <div>
-      <nav class='navbar navbar-dark bg-dark footer'>Footer</nav>
+      <nav class='navbar navbar-dark bg-dark footer'>
+        <img className='social-media-icons mx-1' src={email} />
+        <img className='social-media-icons mx-1' src={facebook} />
+        <img className='social-media-icons mx-1' src={twitter} />
+        <img className='social-media-icons mx-1' src={youtube} />
+        <img className='social-media-icons mx-1' src={donate} />
+      </nav>
     </div>
   );
 }
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -69,25 +69,57 @@ export default function Home({ isMobile }) {
           </Col>
         </Row>
 
-        <Row className='membership mt-5 mb-5'>
-          <div className='col-6'>
-            <p>
-              Membership gives all Lincoln Elementary students in your household
-              access to discounts for some events that the PTO sponsors for our
-              students. Your membership fee helps the PTO fund many educational
-              and social programs throughout the year for our children.
-            </p>
-            <p>
-              Please complete the membership form before proceeding with your
-              payment. Memberships require both form submission and payment.
-              Thank you!
-            </p>
-            <div><span className='membership-link'>Click Here</span></div>
-          </div>
-          <div className='col-6'>
-            <h2 className='title'>Join Us</h2>
-            <img src={school} className='membership-image' />
-          </div>
+        <Row className={`membership mt-5 mb-5 ${isMobile ? 'mobile-row' : ''}`}>
+          {isMobile ? (
+            <>
+              <div className='col'>
+                <h2 className='title'>Join Us</h2>
+                <img src={school} className='membership-image' />
+              </div>
+
+              <div className='col'>
+                <p>
+                  Membership gives all Lincoln Elementary students in your
+                  household access to discounts for some events that the PTO
+                  sponsors for our students. Your membership fee helps the PTO
+                  fund many educational and social programs throughout the year
+                  for our children.
+                </p>
+                <p>
+                  Please complete the membership form before proceeding with
+                  your payment. Memberships require both form submission and
+                  payment. Thank you!
+                </p>
+                <div>
+                  <span className='membership-link'>Click Here</span>
+                </div>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className='col-6'>
+                <p>
+                  Membership gives all Lincoln Elementary students in your
+                  household access to discounts for some events that the PTO
+                  sponsors for our students. Your membership fee helps the PTO
+                  fund many educational and social programs throughout the year
+                  for our children.
+                </p>
+                <p>
+                  Please complete the membership form before proceeding with
+                  your payment. Memberships require both form submission and
+                  payment. Thank you!
+                </p>
+                <div>
+                  <span className='membership-link'>Click Here</span>
+                </div>
+              </div>
+              <div className='col-6'>
+                <h2 className='title'>Join Us</h2>
+                <img src={school} className='membership-image' />
+              </div>
+            </>
+          )}
         </Row>
       </Container>
     </>
