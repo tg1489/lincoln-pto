23362bfd221f52c6ca5d1a0e5d08d266b1341031
Tony <tonyguarino1489@gmail.com>
2023-10-19 12:20:03 -0400

    Improved chalkboard picture with settings

--- client/src/components/PTO.jsx
+++ client/src/components/PTO.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import chalkboard from '../assets/chalkboard.png';
 import '../PTO.css';
 
 export default function PTO() {
   return (
     <div className='app-container'>
       <div className='content'>
-        <h1>PTO Meeting Schedule 2023-2024</h1>
-
-        <Container className='chalkboard'>
-          <Row className='line-1'>Test</Row>
+        <Container>
+          <h1 className='pto-title'>
+            PTO Meeting Schedule <span className='pto-year'>2023-24</span>
+          </h1>
+          <div className='chalkboard'>
+            <Row className='line-1'>Hidden</Row>
+            <Row className='line-2'>
+              6:00 PM in Lincoln School Multipurpose Room
+            </Row>
+            <Row className='line-3'>Test</Row>
+            <Row className='line-4'>December 6th</Row>
+            <Row className='line-5'>February 22nd</Row>
+            <Row className='line-6'>Test</Row>
+            <Row className='line-7'>Additional Dates to Follow</Row>
+          </div>
         </Container>
       </div>
     </div>
